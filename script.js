3d641b63f7cd18e964e75838d55386cc1e3be12d
Senya <10187586+cmrd-senya@users.noreply.github.com>
2021-11-20 23:24:56 +0200

    Initial version: based on speech-color-changer example for Web Speech API

---
+++ script.js
@@ -0,0 +1,58 @@
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent
+
+var recognition = new SpeechRecognition();
+recognition.continuous = false;
+recognition.lang = 'en-US';
+recognition.interimResults = false;
+recognition.maxAlternatives = 1;
+
+var diagnostic = document.querySelector('.output');
+var historyBox = document.querySelector('#history');
+var hints = document.querySelector('.hints');
+
+hints.innerHTML = 'Tap/click then say a word you want to look up.'
+
+document.body.onclick = function() {
+  recognition.start();
+  console.log('Ready to receive a color command.');
+}
+
+recognition.onresult = function(event) {
+  const translationBox = document.querySelector('#translationBox');
+  // The SpeechRecognitionEvent results property returns a SpeechRecognitionResultList object
+  // The SpeechRecognitionResultList object contains SpeechRecognitionResult objects.
+  // It has a getter so it can be accessed like an array
+  // The first [0] returns the SpeechRecognitionResult at the last position.
+  // Each SpeechRecognitionResult object contains SpeechRecognitionAlternative objects that contain individual results.
+  // These also have getters so they can be accessed like arrays.
+  // The second [0] returns the SpeechRecognitionAlternative at position 0.
+  // We then return the transcript property of the SpeechRecognitionAlternative object
+  const { transcript, confidence } = event.results[0][0];
+
+  historyBox.textContent += ' ' + transcript
+  diagnostic.textContent = 'Result received: ' + transcript + '.';
+  console.log('Confidence: ' + confidence);
+  translationBox.src =
+    "https://context.reverso.net/%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4/%D0%B0%D0%BD%D0%B3%D0%BB%D0%B8%D0%B9%D1%81%D0%BA%D0%B8%D0%B9-%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%B8%D0%B9/"
+    + encodeURIComponent(transcript)
+}
+
+recognition.onspeechend = function() {
+  recognition.stop();
+}
+
+recognition.onnomatch = function(event) {
+  diagnostic.textContent = "I didn't recognise that color.";
+}
+
+recognition.onerror = function(event) {
+  diagnostic.textContent = 'Error occurred in recognition: ' + event.error;
+}
+
+recognition.onend = function(event) {
+  setTimeout(() => {
+    recognition.start()
+  }, 0)
+}
